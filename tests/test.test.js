5ff4cd6877452997bcba8e6b90833f5cf2555bd2
Ernesto Pestaña <epestanamx@gmail.com>
2018-08-24 20:20:42 -0500

    Prueba de falla

--- tests/test.test.js
+++ tests/test.test.js
@@ -4,4 +4,8 @@ describe('Prueba de ejemplo', () => {
   it('Esta prueba debería pasar', () => {
     chai.expect(1).to.equal(1);
   })
+  
+  it('Esta prueba debería fallar', () => {
+    chai.expect(1).to.equal(0);
+  })
 })
